ac65d2a65a3f9ff1529c741be44039adf084af03
lazarok09 <lazarok09@gmail.com>
2023-06-30 17:01:55 -0300

    refactor(Chat): add main tag

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -1,12 +1,11 @@
 import { DialogComponent } from "../Dialog";
-import styles from "./index.module.scss";
+
 import { RealTimeChat } from "./real";
 export const Chat = () => {
   return (
-    <div>
-      <h1 className={styles.man}>Hello World</h1>
+    <main>
       <DialogComponent />
       <RealTimeChat />
-    </div>
+    </main>
   );
 };
